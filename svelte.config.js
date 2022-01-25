c6258e9e8dbe0f23f2411d33135f6ae01d23d1f0
Ari Perkkiö <ari.perkkio@gmail.com>
2022-01-25 17:53:35 +0000

    build: fix empty build

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: preprocess(),
     kit: {
         adapter: adapter(),
         target: '#svelte',
-        paths: {
-            base: dev ? '' : '/html-preview',
-        },
         appDir: 'internal',
         vite: {
             server: {
