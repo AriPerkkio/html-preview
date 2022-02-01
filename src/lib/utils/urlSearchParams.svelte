516ffeb97777ab5afdeed0393d2b17ca5db3c98b
Ari Perkkiö <ari.perkkio@gmail.com>
2022-02-01 18:21:07 +0000

    refactor: state import/export methods to utils

---
+++ src/lib/utils/urlSearchParams.svelte
@@ -0,0 +1,75 @@
+<script lang="ts" context="module">
+    import { browser } from '$app/env';
+    import { base } from '$app/paths';
+
+    import type { EditorType } from '$lib/CodeEditors.svelte';
+
+    interface InitState {
+        editors: EditorType[];
+        style: string;
+    }
+
+    export function initializeFromUrlSearchParams(): InitState {
+        const defaultValues = {
+            editors: [{ id: 1, code: '' }],
+
+            // Svelte seems to break if style tag is written in string directly, sveltejs/svelte#6923
+            style: '<' + `style>\n  \n</style>`,
+        };
+
+        if (!browser) {
+            return defaultValues;
+        }
+
+        const encoded = new URLSearchParams(document.location.search).get(
+            'code'
+        );
+        let json: unknown;
+
+        if (encoded) {
+            try {
+                const decoded = atob(decodeURIComponent(encoded));
+                json = JSON.parse(decoded);
+            } catch (e) {
+                console.error('Unable to parse url search parameters', e);
+            }
+        }
+
+        if (hasProp(json, 'style') && typeof json.style === 'string') {
+            if (hasProp(json, 'editors') && Array.isArray(json.editors)) {
+                if (json.editors.every(isEditor)) {
+                    return { style: json.style, editors: json.editors };
+                }
+            }
+        }
+
+        return defaultValues;
+    }
+
+    export function buildStatefulUrl(state: InitState): URL {
+        const encoded = btoa(JSON.stringify(state));
+        const url = new URL(base, window.location.origin);
+
+        url.searchParams.set('code', encodeURIComponent(encoded));
+
+        return url;
+    }
+
+    function isEditor(editor: unknown): editor is EditorType {
+        return (
+            hasProp(editor, 'id') &&
+            typeof editor.id === 'number' &&
+            hasProp(editor, 'code') &&
+            typeof editor.code === 'string'
+        );
+    }
+
+    function hasProp<K extends string>(
+        obj: any,
+        key: K
+    ): obj is Record<K, unknown> {
+        if (!obj) return false;
+
+        return key in obj;
+    }
+</script>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
     import { base } from '$app/paths';
-    import { browser } from '$app/env';
-    import CodeEditors, { EditorType } from '$lib/CodeEditors.svelte';
+    import CodeEditors from '$lib/CodeEditors.svelte';
     import StyleEditor from '$lib/StyleEditor.svelte';
+    import {
+        buildStatefulUrl,
+        initializeFromUrlSearchParams,
+    } from '$lib/utils/urlSearchParams.svelte';
 
     const refs: { sandbox?: HTMLIFrameElement } = {};
     let exportUrl: string | undefined;
 
-    type InitState = { editors: EditorType[]; style: string };
-    let { editors, style }: InitState = initialize();
+    let { editors, style } = initializeFromUrlSearchParams();
+
+    $: {
+        refs.sandbox?.contentWindow?.postMessage({
+            type: 'STYLE_CHANGE',
+            value: style,
+        });
+    }
 
     function domTreeChanged(event: CustomEvent) {
         const { value } = event.detail;
@@ -19,79 +28,11 @@
         });
     }
 
-    function styleChanged(event: CustomEvent) {
-        const { value } = event.detail;
-
-        refs.sandbox?.contentWindow?.postMessage({
-            type: 'STYLE_CHANGE',
-            value,
-        });
-    }
-
     function onExport() {
-        if (!browser) return;
+        const url = buildStatefulUrl({ style, editors });
 
-        const encoded = btoa(JSON.stringify({ editors, style }));
-        const url = new URL(base, window.location.origin);
-
-        url.searchParams.set('code', encodeURIComponent(encoded));
         window.history.pushState({}, '', url);
         exportUrl = url.toString();
-    }
-
-    function initialize(): InitState {
-        const defaultValues = {
-            editors: [{ id: 1, code: '' }],
-
-            // Svelte seems to break if style tag is written in string directly, sveltejs/svelte#6923
-            style: '<' + `style>\n  \n</style>`,
-        };
-
-        if (!browser) {
-            return defaultValues;
-        }
-
-        const encoded = new URLSearchParams(document.location.search).get(
-            'code'
-        );
-        let json: unknown;
-
-        if (encoded) {
-            try {
-                const decoded = atob(decodeURIComponent(encoded));
-                json = JSON.parse(decoded);
-            } catch (e) {
-                console.error('Unable to parse url search parameters', e);
-            }
-        }
-
-        if (hasProp(json, 'style') && typeof json.style === 'string') {
-            if (hasProp(json, 'editors') && Array.isArray(json.editors)) {
-                if (json.editors.every(isEditor)) {
-                    return { style: json.style, editors: json.editors };
-                }
-            }
-        }
-
-        return defaultValues;
-    }
-
-    function isEditor(editor: unknown): editor is EditorType {
-        return (
-            hasProp(editor, 'id') &&
-            typeof editor.id === 'number' &&
-            hasProp(editor, 'code') &&
-            typeof editor.code === 'string'
-        );
-    }
-
-    function hasProp<K extends string>(
-        obj: any,
-        key: K
-    ): obj is Record<K, unknown> {
-        if (!obj) return false;
-
-        return key in obj;
     }
 </script>
 
@@ -110,7 +51,7 @@
             <a href={exportUrl}>{exportUrl}</a>
         {/if}
 
-        <StyleEditor on:change={styleChanged} bind:code={style} />
+        <StyleEditor bind:code={style} />
         <CodeEditors on:change={domTreeChanged} bind:editors />
     </div>
 </div>
