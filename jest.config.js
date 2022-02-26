6eb21b0357da8ab46d32ec4ec6d9a08aa145d6b1
Ari Perkkiö <ari.perkkio@gmail.com>
2022-02-26 11:13:42 +0000

    test: remove out-dated test script references

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').InitialOptionsTsJest} */
-const config = {
-    testEnvironment: 'jsdom',
-    preset: 'ts-jest',
-    verbose: true,
-    roots: ['test'],
-};
-
-export default config;
