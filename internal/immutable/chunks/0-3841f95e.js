2a0f0a2df325ac545c46eb632edfc4fb0b0acf13
AriPerkkio <ari.perkkio@gmail.com>
2023-02-12 12:32:34 +0200

    Updates

---
+++ internal/immutable/chunks/0-3841f95e.js
@@ -0,0 +1,4 @@
+import { default as default2 } from "../components/layout.svelte-590881eb.js";
+export {
+  default2 as component
+};
---
+++ internal/immutable/chunks/2-a32487f9.js
@@ -0,0 +1,6 @@
+import { _ } from "./_page-de0ef88a.js";
+import { default as default2 } from "../components/pages/_page.svelte-227fc509.js";
+export {
+  default2 as component,
+  _ as universal
+};
